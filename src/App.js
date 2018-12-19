26c07cb38dfea195befcd448417fa922804a0396
androidpirate <emrekalaycidesign@gmail.com>
2018-12-19 11:37:11 -0500

    Add google map script and load it in componentDidMount()

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+  componentDidMount() {
+    this.loadScript()
+  }
+
+  // Creates and returns a script tag for Google Maps API
+  createMapScript() {
+    let mapScript = document.createElement("script")
+    const API_KEY = "AIzaSyAuh6WJvj8_TW_iwVKyM9n6yedBAMLp06A"
+    mapScript.src = `https://maps.googleapis.com/maps/api/js?key=${API_KEY}&v=3&callback=initMap`
+    mapScript.async = true
+    mapScript.defer = true
+    mapScript.onerror = function () {
+        document.write("Google Maps can't be loaded");
+    };
+    return mapScript
+  }
+
+  // Loads map script to the body and calls initMap() function to initialize the map
+  loadScript() {
+    let mapScript = this.createMapScript()
+    let firstScriptElement = document.getElementsByTagName("script")[0]
+    firstScriptElement.parentNode.insertBefore(mapScript, firstScriptElement)
+    window.initMap = this.initMap
+  }
+
+  // Initializes a map element using Google Maps API
+  initMap() {
+    let map = new window.google.maps.Map(document.getElementById('map'), {
+      center: { lat: 42.2808, lng: -83.7330 },
+      zoom: 14
+    })
+    window.map = map
+    let infowindow = new window.google.maps.InfoWindow()
+    window.infowindow = infowindow
+    window.isInfoWindowOpen = false
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div id="map"></div>
       </div>
     );
   }
