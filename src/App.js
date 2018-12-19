7ed06febc9b30d78a3fdfa174116cdee58e37ca4
androidpirate <emrekalaycidesign@gmail.com>
2018-12-19 11:54:56 -0500

    Add Header and Content components to App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 import './App.css';
+import Header from './components/Header'
+import Content from './components/Content'
 
 class App extends Component {
 
@@ -43,7 +45,8 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <div id="map"></div>
+        <Header/>
+        <Content/>
       </div>
     );
   }
