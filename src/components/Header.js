13ccb4c21c4cebc91597c303de5c90492c9f68ec
androidpirate <emrekalaycidesign@gmail.com>
2018-12-19 11:46:05 -0500

    Add Header component

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class Header extends Component {
+  render() {
+    return (
+      <div id="map-header">
+        <p id="header-text">Neighborhood Map Header</p>
+      </div>
+    )
+  }
+}
+
+export default Header
